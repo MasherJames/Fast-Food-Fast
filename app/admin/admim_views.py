833aea6b58895cc28ea766dacd63259d410e476f
James Macharia <jamesmasher13@gmail.com>
2018-09-04 20:23:35 +0300

    get specific order

--- app/admin/admim_views.py
+++ app/admin/admim_views.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource,reqparse
-from models.models import FoodItem, FoodItems
+from models.models import FoodItem, FoodItems, FoodOrder
 
 class Foods(Resource):
     parser = reqparse.RequestParser()
@@ -23,7 +23,34 @@ class Foods(Resource):
 
     def get(self):
         """ Get all food items """
-        return {"Food Items": [food_item.serialize() for food_item in FoodItems]}
+        return {"Food Items": [food_item.serialize() for food_item in FoodItems]}, 200
+
+class SpecificOrder(Resource):
+
+    def get(self, food_order_id):
+        """ Fetch a specific order """
+        food_order = FoodOrder().get_by_id(food_order_id)
+
+        if not food_order:
+            return {"message":"Food order does not exist"}, 404
+
+        return food_order.serialize(), 200
+
+    def put(self, food_order_id):
+        """ Update the status of an order """
+        food_order = FoodOrder().get_by_id(food_order_id)
+
+        if not food_order:
+            return {"message":"Food order does not exist"}, 404
+
+        if food_order.status == "pending":
+            food_order.status = "approved"
+
+        return food_order.serialize(), 200
+
+
+
+
 
 
 
