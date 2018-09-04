a2f6e0de534d25419faafe2750408c2090a6810d
James Macharia <jamesmasher13@gmail.com>
2018-09-04 20:21:56 +0300

    register order blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_restful import Api
 from instance.config import app_config
-from .admin.admim_views import Foods
+from .admin.admim_views import Foods, SpecificOrder
+from .customer.customer_views import PostOrders, GetOrders
+
 
 def create_app(config_mode):
     app = Flask(__name__, instance_relative_config=True)
@@ -12,5 +14,14 @@ def create_app(config_mode):
     admin = Api(admin_blp)
     app.register_blueprint(admin_blp, url_prefix="/api/v1")
 
+    from .customer import customer_blueprint as customer_bp
+    customer = Api(customer_bp)
+    app.register_blueprint(customer_bp, url_prefix="/api/v1")
+
+
     admin.add_resource(Foods, '/fooditems')
+    admin.add_resource(SpecificOrder, '/orders/<int:food_order_id>')
+
+    customer.add_resource(PostOrders, '/orders/<int:food_id>')
+    customer.add_resource(GetOrders, '/orders')
     return app
