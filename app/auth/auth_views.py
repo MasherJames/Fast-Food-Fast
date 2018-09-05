bccdd43c8c71e008ea17d88e9b201a092544589b
James Macharia <jamesmasher13@gmail.com>
2018-09-05 20:50:46 +0300

    add auth views

---
+++ app/auth/auth_views.py
@@ -0,0 +1,82 @@
+import datetime
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource, reqparse
+from werkzeug.security import check_password_hash
+from models.models import User, users
+from utils import validators
+
+
+class SignUp(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('username', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument('email', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument('password', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument("is_admin", type=int, required=True,
+                        help='This field cannot be left blank')
+
+    def post(self):
+        """ SignUp a new user """
+        request_data = SignUp.parser.parse_args()
+
+        username = request_data['username']
+        email = request_data['email']
+        password = request_data['password']
+        is_admin = request_data['is_admin']
+
+        if not validators.Validators().valid_username(username):
+            return {"message": "Invalid username"}, 400
+
+        if not validators.Validators().valid_email(email):
+            return {"message": "Invalid email, enter a valid email"}, 400
+
+        if not validators.Validators().valid_password(password):
+            return {"message": "Enter a valid password"}, 400
+
+        if type(is_admin) is not int:
+            return {"message": "value should be 0 or 1 only"}
+
+        if is_admin not in range(0, 2):
+            return {"message": "can only be 0 for normal user and 1 for admin"}, 400
+
+        if User().get_by_username(username):
+            return {"message": f"User with username {username} already exists"}, 400
+
+        if User().get_by_email(email):
+            return {"message": f"User with email {email} already exists"}, 400
+
+        user = User(username, email, password, bool(is_admin))
+
+        users.append(user)
+        print(user.is_admin)
+
+        return {"message": f"Account for {username} has been created successfully"}, 201
+
+
+class Login(Resource):
+
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('username', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument('password', type=str, required=True,
+                        help='This field cannot be left blank')
+
+    def post(self):
+        """ Log in a signed up user """
+        request_data = Login.parser.parse_args()
+
+        username = request_data['username']
+        password = request_data['password']
+
+        user = User().get_by_username(username)
+
+        if user and check_password_hash(user.password_hash, password):
+            expires = datetime.timedelta(minutes=30)
+            token = create_access_token(
+                user.username, user.is_admin, expires_delta=expires)
+            return {'token': token, 'message': f'You were successfully logged in {username}'}, 200
+        return {'message': 'user not found'}, 404
