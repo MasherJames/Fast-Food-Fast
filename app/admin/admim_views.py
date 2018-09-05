bd77b6a42457ad4e35d3b2e011cdcbc92fa9351e
James Macharia <jamesmasher13@gmail.com>
2018-09-05 20:49:03 +0300

    add admin views

--- app/admin/admim_views.py
+++ app/admin/admim_views.py
@@ -1,12 +1,19 @@
-from flask_restful import Resource,reqparse
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_current_user
 from models.models import FoodItem, FoodItems, FoodOrder
+from utils import validators
+
 
 class Foods(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name', type=str, required=True, help='This field cannot be left blank')
-    parser.add_argument('description', type=str, required=True, help='This field cannot be left blank')
-    parser.add_argument('price', type=int, required=True, help='This field cannot be left blank')
+    parser.add_argument('name', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument('description', type=str, required=True,
+                        help='This field cannot be left blank')
+    parser.add_argument('price', type=int, required=True,
+                        help='This field cannot be left blank')
 
+    @jwt_required
     def post(self):
         """ Create a new food item """
         request_data = Foods.parser.parse_args()
@@ -15,42 +22,46 @@ class Foods(Resource):
         description = request_data['description']
         price = request_data['price']
 
+        if not validators.Validators().valid_inputs(name):
+            return {"message": "Enter a valid food name"}, 400
+
+        if not validators.Validators().valid_inputs(description):
+            return {"message": "Enter valid food description"}, 400
+
         food_item = FoodItem(name, description, price)
 
         FoodItems.append(food_item)
 
-        return {"message":"Food item created successfully"}, 201
+        return {"message": "Food item created successfully"}, 201
 
+    @jwt_required
     def get(self):
         """ Get all food items """
         return {"Food Items": [food_item.serialize() for food_item in FoodItems]}, 200
 
+
 class SpecificOrder(Resource):
 
+    @jwt_required
     def get(self, food_order_id):
         """ Fetch a specific order """
         food_order = FoodOrder().get_by_id(food_order_id)
 
         if not food_order:
-            return {"message":"Food order does not exist"}, 404
+            return {"message": "Food order does not exist"}, 404
 
         return food_order.serialize(), 200
 
+    @jwt_required
     def put(self, food_order_id):
         """ Update the status of an order """
+
         food_order = FoodOrder().get_by_id(food_order_id)
 
         if not food_order:
-            return {"message":"Food order does not exist"}, 404
+            return {"message": "Food order does not exist"}, 404
 
         if food_order.status == "pending":
             food_order.status = "approved"
 
         return food_order.serialize(), 200
-
-
-
-
-
-
-
