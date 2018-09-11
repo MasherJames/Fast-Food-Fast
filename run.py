9be85ddc9c040953c6bfe561788c71c92685434c
James Macharia <jamesmasher13@gmail.com>
2018-09-11 19:02:45 +0300

    cli funcs

--- run.py
+++ run.py
@@ -1,7 +1,26 @@
 import os
+import click
 from app import create_app
+from models.models import FastFoodsDb, User, FoodItem, FoodOrder
 
 app = create_app(os.getenv("APP_SETTINGS") or 'default')
+
+
+@app.cli.command()
+def migrate():
+    FastFoodsDb().init_app(app)
+    User().create_table()
+    FoodItem().create_table()
+    FoodOrder().create_table()
+
+
+@app.cli.command()
+def drop():
+    FastFoodsDb().init_app(app)
+    User().drop_table()
+    FoodItem().drop_table()
+    FoodOrder().drop_table()
+
 
 if __name__ == '__main__':
     app.run()
