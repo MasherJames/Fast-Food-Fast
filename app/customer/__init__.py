bfe96017e7af5769819c66f6fdd1a859242ba63c
James Macharia <jamesmasher13@gmail.com>
2018-09-12 09:17:11 +0300

    Add a blue print

--- app/customer/__init__.py
+++ app/customer/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
-from .customer_views import PostOrders, GetOrders, SpecificItem, SpecificCustomerOrders
+from .customer_views import PostOrders, GetOrders, SpecificCustomerOrders
 customer_blueprint = Blueprint("customer", __name__)
