e12c40b344a36bf8e4fcf50f215824a9956836c1
James Macharia <jamesmasher13@gmail.com>
2018-09-12 19:57:35 +0300

    Add update status to blueprint

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
-from .admim_views import Foods, SpecificOrder, SpecificItem
+from .admim_views import Foods, SpecificOrder, SpecificItem, AcceptOrder, DeclineOrder, CompleteOrder, AcceptedOrders, DeclinedOrders, CompletedOrders
 
 admin_blueprint = Blueprint('admin', __name__)
